299f1ebeeaf12a81412eb566b0a48277a8837562
nageshwarbr <nageshwarburman@gmail.com>
2019-12-09 15:57:18 +0530

    all databses

---
+++ Database/elf_employee_db_empinfo.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
+--
+-- Host: localhost    Database: elf_employee_db
+-- ------------------------------------------------------
+-- Server version	5.7.28-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `empinfo`
+--
+
+DROP TABLE IF EXISTS `empinfo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `empinfo` (
+  `emo_id` int(11) NOT NULL,
+  `emp_name` varchar(50) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `salary` double DEFAULT NULL,
+  `designation` varchar(20) DEFAULT NULL,
+  `mobile` bigint(20) DEFAULT NULL,
+  `password` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `empinfo`
+--
+
+LOCK TABLES `empinfo` WRITE;
+/*!40000 ALTER TABLE `empinfo` DISABLE KEYS */;
+INSERT INTO `empinfo` VALUES (103,'Mark',53,84555,'SSD',44444444,'qwerty'),(104,'Celena',43,105555,'JD',12345567,'qwerty'),(107,'John',83,785555,'SD',665286652,'qwerty'),(112,'Alak',34,344566,'JD',4444444444444,'qwerty'),(125,'jh',66,78979,'hjk',34364,'qwerty'),(144,'ghfhgf',22,7689657,'hjl',5764534523412,'qwerty'),(166,'dgreg',34,34325,'fdghf',234543657,'qwerty'),(234,'aaa',23,4545454,'sd',34255555556,'qwerty');
+/*!40000 ALTER TABLE `empinfo` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-12-09 15:53:04
